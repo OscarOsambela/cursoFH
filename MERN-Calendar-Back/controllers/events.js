79f05da494388e01090e3d497f010420c17e8659
Oscar Osambela <osambelacolareta@gmail.com>
2022-03-05 00:24:21 -0500

    eventos finalizados

--- MERN-Calendar-Back/controllers/events.js
+++ MERN-Calendar-Back/controllers/events.js
@@ -27,18 +27,71 @@ const createEvents = async (req, res) => {
   }
 };
 
-const updateEvents = (req, res) => {
-  res.json({
-    ok: true,
-    msg: "actualizar eventos",
-  });
+const updateEvents = async (req, res) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const newEvent = await Events.findById(eventId);
+    if (!newEvent) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Event don´t exist",
+      });
+    }
+    if (newEvent.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "You don´t permission for changes in events",
+      });
+    }
+    const updateEvent = {
+      ...req.body,
+      user: uid,
+    };
+    const eventUpdated = await Events.findOneAndUpdate(eventId, updateEvent, {
+      new: true,
+    });
+    res.json({
+      ok: true,
+      eventId: eventUpdated,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please connect with your administrator",
+    });
+  }
 };
 
-const deleteEvents = (req, res) => {
-  res.json({
-    ok: true,
-    msg: "eliminar eventos",
-  });
+const deleteEvents = async (req, res) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const delEvent = await Events.findById(eventId);
+    if (!delEvent) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Event don´t exist",
+      });
+    }
+    if (delEvent.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "You don´t permission for delete events",
+      });
+    }
+    await Events.findByIdAndDelete(eventId);
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please connect with your administrator",
+    });
+  }
 };
 
 module.exports = {
